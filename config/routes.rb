8510a04d3c28993b8ea0967b70c9087de0c8befe
Hayato Clarke <hayatoclarke@gmail.com>
2020-08-17 19:00:30 +0900

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :projects, only: %i[ index show ]
+  get 'about', to: 'pages#about'
+  get 'hello', to: 'pages#hello'
 end
